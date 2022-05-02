d0d9ddc865b5226e34d0d3eba4090563413a8379
Pabst, Uwe (DI FA CTR SVC PRC1) <uwe.pabst@siemens.com>
2022-05-02 16:14:42 +0200

    added new tool fro registering webserver on a remote target

---
+++ RegisterWebserver/RegisterWebserver/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Security;
+
+namespace RegisterWebserver
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool isRegistered = true;
+
+            if (args[0] == "verify")
+            {
+                using (Process pcsystemControl = new Process())
+                {
+                    pcsystemControl.StartInfo.FileName = @"powershell";
+                    pcsystemControl.StartInfo.Arguments = "C:\\Users\\pu3846\\Documents\\Playground\\VerifyWebserver.ps1 -Computername " + args[1];
+                    pcsystemControl.StartInfo.UseShellExecute = false;
+                    pcsystemControl.StartInfo.RedirectStandardOutput = true;
+
+                    pcsystemControl.Start();
+
+                    string message = pcsystemControl.StandardOutput.ReadToEnd();
+                    Console.WriteLine(message);
+
+                    string[] seperators = new string[] { "\r\n" };
+                    string[] lineArray = message.Split(seperators, StringSplitOptions.None);
+
+                    foreach (string line in lineArray)
+                    {
+                        if (line.Contains("Warning"))
+                        {
+                            if (line.Contains("AnalyseSimaticApplication"))
+                            {
+                                isRegistered = false;
+                            }
+                        }
+                    }
+
+                    pcsystemControl.WaitForExit();
+                    Console.WriteLine("Exit Code = " + pcsystemControl.ExitCode.ToString());
+                }
+
+                if (isRegistered)
+                {
+                    Console.WriteLine("Webserver is already registered");
+                }
+                else
+                {
+                    Console.WriteLine("Webserver is not registered");
+                }
+            }
+            else if (args[0] == "register")
+            {
+                using (Process pcsystemControl = new Process())
+                {
+                    pcsystemControl.StartInfo.FileName = @"powershell";
+                    pcsystemControl.StartInfo.Arguments = "C:\\Users\\pu3846\\Documents\\Playground\\RegisterWebserver.ps1 -Computername " + args[1];
+                    pcsystemControl.StartInfo.UseShellExecute = false;
+                    pcsystemControl.StartInfo.RedirectStandardOutput = true;
+
+                    pcsystemControl.Start();
+
+                    string message = pcsystemControl.StandardOutput.ReadToEnd();
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
